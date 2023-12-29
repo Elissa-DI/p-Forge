1800b16d37ab9ae1f545c3ccf3d87a19625ed9cb
DebugBeast <dusabeelissa05@gmail.com>
2023-12-29 02:20:06 +0200

    new changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -7,11 +9,13 @@ const Home = () => {
         <span className="orange_gradient text-center">
           AI-Powered Prompts
         </span>
-        <p className='desc text-center'>
-          PromptForge is an open-source AI prompting tool for modern world to
-          discover, create and share creative prompts
-        </p>
       </h1>
+      <p className='desc text-center'>
+        PromptForge is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      <Feed />
     </section>
   )
 }
