7a3c68863ae1c99eb3e8f2e6d530bf18ef298140
DebugBeast <dusabeelissa05@gmail.com>
2024-01-01 01:50:59 +0200

    new changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-const route = () => {
-  return (
-    <div>route</div>
-  )
-}
+// import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
-export default route
+console.log({
+  clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+});
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ], 
+  async session({ session }) {
+
+  },
+  async signIn([ prfoile ]) {
+    try {
+      await connectToDB();
+      //Check if a user exists
+
+      //If not, create a new user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+
+  }
+})
