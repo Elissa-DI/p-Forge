93c5058a3957db1016e8dc31b6098d01636baeb9
DebugBeast <dusabeelissa05@gmail.com>
2024-01-05 00:30:48 +0200

    new changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,17 +6,19 @@ import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const { sata: session } = useSession();
+
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
 
   const [toggleDropdown, setToggleDropdown] = useState(false)
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
       setProviders(response);
     }
 
-    setProviders();
+    setUpProviders();
   }, [])
 
   return (
@@ -32,8 +34,10 @@ const Nav = () => {
         <p className='logo_text'>PromptForge</p>
       </Link>
 
+      {/* {alert(providers)}  */}
+
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href="/create-prompt" className='black_btn'>
               Create Post
@@ -70,7 +74,7 @@ const Nav = () => {
       </div>
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
