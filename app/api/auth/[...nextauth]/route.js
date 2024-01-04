fb2468c5fb15adbf9c067251c200e0b2bfbf2e71
DebugBeast <dusabeelissa05@gmail.com>
2024-01-04 23:53:16 +0200

    new changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,13 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-// import User from '@models/user';
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
+import Profile from '@components/Profile';
 
 console.log({
   clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 });
 
 const handler = NextAuth({
@@ -15,16 +16,29 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ], 
+  ],
   async session({ session }) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+    session.user.id = sessionUser._id.toString();
 
+    return session;
   },
-  async signIn([ prfoile ]) {
+  async signIn([prfoile]) {
     try {
       await connectToDB();
       //Check if a user exists
+      const UserExists = await User.findOne({
+        email: Profile.email
+      })
 
       //If not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
       return true;
     } catch (error) {
