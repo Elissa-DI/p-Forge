cf8d0c982b73c41c152e5b4c7038bff75d92b1da
Elissa-DI <elissadusabe@gmail.com>
2024-01-05 21:14:02 +0200

    new changes

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+"use client";
 
-const PromptCard = () => {
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { FaCheck, FaCopy } from 'react-icons/fa6'
+
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
+  const [copied, setCopied] = useState("")
   return (
-    <div>PromptCard</div>
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div
+          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+        // onClick={handleProfileClick}
+        >
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-santoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div className="copy_btn" onClick={() => { }}>
+          <Image
+            src={copied === post.prompt
+              ? '/assets/icons/tick.svg'
+              : '/assets/icons/copy.svg'
+              // ? <FaCheck size={11} color="black"/>
+              // : <FaCopy size={11} color="black"/>
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <p className="my-4 font-santoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+      onClick={() => handleTagClick && handleTagClick(post.tag)}
+        className="font-inter text-sm blue_gradient cursor-pointer"
+      >
+        {post.tag}
+      </p>
+    </div>
   )
 }
 
