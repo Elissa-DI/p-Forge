3c4c039ab3f022b12a592433a375a5b082ba047c
Elissa-DI <elissadusabe@gmail.com>
2024-01-10 19:05:54 +0200

    new changes

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -45,10 +45,16 @@ export const DELETE = async (request, { params }) => {
         await connectToDB(); 
 
         // Find the prompt by ID and remove it
-        await Prompt.findByIdAndRemove(params.id);
+        const deletedPrompt = await Prompt.findOneAndDelete({ _id: params.id });
 
+        if (!deletedPrompt) {
+            return new Response("Prompt not found", { status: 404 });
+        }
+
+        console.log("Prompt deleted successfully⚠️");
         return new Response("Prompt deleted successfully", { status: 200 });
     } catch (error) {
         return new Response("Error deleting prompt", { status: 500 });
     }
 };
+
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,13 +27,20 @@ const MyProfile = () => {
     }
 
     const handleDelete = async (post) => {
-        const hasConfirmed = confirm("Are you sure you want to delete thid prompt");
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt🤔");
 
         if(hasConfirmed) {
             try {
+                await fetch(`/api/prompt/${post._id.toString()}`, {
+                    method: "DELETE"
+                });
                 
+                const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+                setPosts(filteredPosts);
+                console.log("post successfully deleted🤘");
             } catch (error) {
-                
+                console.log(error);                
             }
         }
     }
