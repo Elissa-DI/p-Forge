b941a6780bff5bb20365e23b3416a5b82f7f7f02
Elissa-DI <elissadusabe@gmail.com>
2024-01-17 21:22:50 +0200

    new changes

--- components/Form.jsx
+++ components/Form.jsx
@@ -11,7 +11,34 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         imagination run wild with any AI-powered platform
       </p>
 
-      
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your post here'
+            required
+            className='form_textarea '
+          />
+        </label>
+
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Field of Prompt{" "}
+            <span className='font-normal'>
+              (#product, #webdevelopment, #idea, etc.)
+            </span>
+          </span>
+          
+        </div>
+      </form>
     </section>
   );
 };
