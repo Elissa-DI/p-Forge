796c596a09f48c6fe50e5b933dde9edfbef143ef
Elissa-DI <elissadusabe@gmail.com>
2024-01-18 21:04:28 +0200

    new changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,53 +1,53 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const { sata: session } = useSession();
+  const { data: session } = useSession();
 
-  const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  const [toggleDropdown, setToggleDropdown] = useState(false)
   useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    }
-
-    setUpProviders();
-  }, [])
-
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
+ 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href="/" className='flex gap2 flex-center'>
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src="/assets/images/logo.svg"
-          alt='PromptForge logo'
+          src='/assets/images/logo.svg'
+          alt='logo'
           width={30}
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>PromptForge</p>
+        <p className='logo_text'>Promptopia</p>
       </Link>
 
-      {/* {alert(providers)}  */}
+      {/* {alert(providers)} */}
 
-      <div className="sm:flex hidden">
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
         {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href="/create-prompt" className='black_btn'>
+            <Link href='/create-prompt' className='black_btn'>
               Create Post
             </Link>
+
             <button type='button' onClick={signOut} className='outline_btn'>
               Sign Out
             </button>
-            <Link href="/profile">
+
+            <Link href='/profile'>
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -62,46 +62,47 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
-              )
-              )}
+              ))}
           </>
         )}
       </div>
+
       {/* Mobile Navigation */}
-      <div className="sm:hidden flex relative">
+      <div className='sm:hidden flex relative'>
         {session?.user ? (
-          <div className="flex">
+          <div className='flex'>
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
-              className='cursor-pointer rounded-full'
+              className='rounded-full'
               alt='profile'
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
 
             {toggleDropdown && (
-              <div className="dropdown">
+              <div className='dropdown'>
                 <Link
-                  href="/profile"
+                  href='/profile'
                   className='dropdown_link'
                   onClick={() => setToggleDropdown(false)}
                 >
                   My Profile
                 </Link>
                 <Link
-                  href="/create-prompt"
+                  href='/create-prompt'
                   className='dropdown_link'
                   onClick={() => setToggleDropdown(false)}
                 >
                   Create Prompt
                 </Link>
-
                 <button
                   type='button'
                   onClick={() => {
@@ -122,19 +123,19 @@ const Nav = () => {
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
-              )
-              )}
+              ))}
           </>
         )}
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false;//Track the connection
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
